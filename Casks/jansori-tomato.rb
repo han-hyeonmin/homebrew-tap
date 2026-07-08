6ad0b355ac0d61a2a5977532670c60c6d82a5d07
Hyeonmin HAN <hanhyeonmin@kaist.ac.kr>
2026-07-08 14:58:52 +0900

    Clean up cask comments

--- Casks/jansori-tomato.rb
+++ Casks/jansori-tomato.rb
@@ -1,10 +1,3 @@
-# Homebrew Cask 템플릿.
-# 개인 tap 저장소(homebrew-tap)의 Casks/ 아래에 두면 아래로 설치된다:
-#   brew install --cask han-hyeonmin/tap/jansori-tomato
-#
-# 릴리즈할 때마다 version / sha256 을 갱신한다:
-#   Scripts/package-release.sh   # sha256 출력
-
 cask "jansori-tomato" do
   version "0.1.1"
   sha256 "3d04b2a30784fef50bec4d97c7ac617895e06e122db143270828b85de30626c6"
@@ -14,7 +7,6 @@ cask "jansori-tomato" do
   desc "Menu bar Pomodoro timer with watching eyes that nag you to focus"
   homepage "https://github.com/han-hyeonmin/jansori-tomato"
 
-  # 새 버전 자동 감지(GitHub 최신 릴리즈 기준).
   livecheck do
     url :url
     strategy :github_latest
